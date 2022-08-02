03282b8f5f5be34575389a09122e5017a602d430
F3879710 Gustavo de Magalhaes Barbosa <gustavo.jay@bb.com.br>
2022-08-02 12:50:10 -0300

    Taxa virou classe e pode inserir novos tipos

--- jaybank/src/main/java/br/com/jaybank/enums/Taxa.java
+++ jaybank/src/main/java/br/com/jaybank/enums/Taxa.java
@@ -8,23 +8,23 @@ import br.com.jaybank.modelos.pessoa.Cliente;
 import br.com.jaybank.modelos.pessoa.PessoaFisica;
 import br.com.jaybank.modelos.pessoa.PessoaJuridica;
 
-public enum Taxa {
-	PJ(
-			0.005,
-			0.005,
-			0.005,
-			0.007),
-	PF(
-			0,
-			0,
-			0.003,
-			0.010);
+public class Taxa implements Serializable {
 
 	private static final long serialVersionUID = 0400000L;
 	private static final Map<Class<? extends Cliente>, Taxa> taxasPorTipo = new HashMap<>();
 	static {
-		taxasPorTipo.put(PessoaFisica.class, PF);
-		taxasPorTipo.put(PessoaJuridica.class, PJ);
+		taxasPorTipo.put(PessoaFisica.class, new Taxa(
+			0,
+			0,
+			0.003,
+			0.010
+		));
+		taxasPorTipo.put(PessoaJuridica.class, new Taxa(
+			0.005,
+			0.005,
+			0.005,
+			0.007
+		));
 	}
 
 	private final double transferencia;
@@ -64,5 +64,12 @@ public enum Taxa {
 		if (resultado == null)
 			throw new IllegalArgumentException("Tipo de taxa não pôde ser determinado.");
 		return resultado;
+	}
+
+	public static <C extends Cliente> boolean adicionaTipoCliente(Class<C> tipoCliente, Taxa novaTaxa) {
+		if (taxasPorTipo.containsKey(tipoCliente))
+			return false;
+		taxasPorTipo.put(tipoCliente, novaTaxa);
+		return true;
 	}
 }
